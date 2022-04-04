64051f65163587e4ddbbcdb6704ad97fdb6d5497
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-04-04 11:31:27 -0300

    fix exerccio02 - 04/04

--- modulo-1/java/EXERCICIO-DE-REPETICAO/src/exercicio02/Exercicio02.java
+++ modulo-1/java/EXERCICIO-DE-REPETICAO/src/exercicio02/Exercicio02.java
@@ -8,16 +8,20 @@ public class Exercicio02 {
 
 		Scanner sc = new Scanner(System.in);
 		int contPares =0;
+		int contImpares =0;
 
 		for(int i = 0; i < 10; i++) {
 			System.out.print("Digite o " + (i+1) + "º número: ");
 			int num = sc.nextInt();
 			if(num % 2 == 0) {
 				contPares++;
+			} else {
+				contImpares++;
 			}
 		}
 		
 		System.out.println("Quantidade de números pares: " + contPares);
+		System.out.println("Quantidade de números ímpares: " + contImpares);
 		
 		sc.close();
 	}
